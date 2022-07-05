44ff3d9ee5afe37dc02c01890fdee68bc379f167
xyz32 <xyz.relativity@gmail.com>
2022-07-05 16:00:27 -0400

    Add some beeping sounds.

--- app/src/main/java/com/adamratana/pshicotimer/MainActivity.java
+++ app/src/main/java/com/adamratana/pshicotimer/MainActivity.java
@@ -3,11 +3,12 @@ package com.adamratana.pshicotimer;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioFormat;
 import android.media.AudioManager;
-import android.media.ToneGenerator;
+import android.media.AudioTrack;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 
 import com.adamratana.pshicotimer.network.TcpClient;
 
@@ -20,8 +21,24 @@ public class MainActivity extends AppCompatActivity implements TcpClient.OnMessa
 
 //		initSensors();
 
-		final ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
-		tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+		findViewById(R.id.triggerButton).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				try {
+					Thread.sleep(1000);
+
+					generateTone(880, 200, 1).play();
+					Thread.sleep(800);
+
+					generateTone(880, 200, 1).play();
+					Thread.sleep(800);
+
+					generateTone(1760, 100, 1).play();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	private void initSensors() {
@@ -41,5 +58,28 @@ public class MainActivity extends AppCompatActivity implements TcpClient.OnMessa
 	@Override
 	public void messageReceived(String message) {
 
+	}
+
+	private AudioTrack generateTone(double freqHz, int durationMs, double volume)
+	{
+		if (volume > 1 || volume < 0){
+			volume = 1; //will make sure it isn't too loud
+		}
+		int rate =
+				AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC);
+
+		int count = (int)(rate * 2.0 * (durationMs / 1000.0)) & ~1;
+		short[] samples = new short[count];
+		for(int i = 0; i < count; i += 2){
+			short sample = (short)(volume * Math.sin(2 * Math.PI * i / (rate / freqHz)) * 0x7FFF);
+			samples[i + 0] = sample;
+			samples[i + 1] = sample;
+		}
+
+		AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC, rate,
+				AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
+				count * (Short.SIZE / 8), AudioTrack.MODE_STATIC);
+		track.write(samples, 0, count);
+		return track;
 	}
 }
